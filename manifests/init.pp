eca9539fb6ba7ef69c2ef22dc4f2204d2e6ef0e5
David Stauffer <david.stauffer@williams-forrest.com>
2016-04-07 20:29:42 -0400

    adding magnolia.properties parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,20 @@
 # ----------
 #
 # $edition is either magnolia 'community' or 'enterprise-pro'
+#
+# magnolia.properties
+# magnolia.home = Root of the webapp's deployment directory.  Optional, default is ${magnolia.app.rootdir}
+# magnolia.repositories.config = Location of repository config file.  Required, default is WEB-INF/config/default/repositories.xml
+# magnolia.repositories.home = Repository home directory.  Optional, default is ${magnolia.home}/repositories
+# magnolia.repositories.jackrabbit.config = Location of Jackrabbit file.  Optional, default is WEB-INF/config/repo-conf/jackrabbit-bundle-derby-search.xml
+# magnolia.logs.dir = Directory where logs are written.  Optional, default is ${magnolia.home}/logs
+# magnolia.cache.startdir = Directory used for cached pages.  Required, default is ${magnolia.home}/cache
+# magnolia.upload.tmpdir = Temporary directory for uploaded files.  Required, default is ${magnolia.home}/tmp
+# magnolia.logs.dir = Directory where logs are written.  Optional, default is ${magnolia.home}/logs
+# magnolia.utf8.enabled = Activate UTF-8 support for pages.  Optional, default is false
+# magnolia.develop = Disables the cache for resources.  To force on-the-fly Sass compiling.  Optional, default is false
+# magnolia.update.auto = Set to true if bootstrapping and update should be performed automatically after installation.  Optional, default is false
+# magnolia.author.key.location = Location of private and public keys used for activation.  Optional, default is ${magnolia.home}/WEB-INF/config/default/magnolia-activation-keypair.properties
 # 
 # Variables
 # ----------
@@ -44,9 +58,21 @@ class magnolia (
   $user                  = $magnolia::params::user,
   $group                 = $magnolia::params::group,
   $install_dir           = $magnolia::params::install_dir,
-
-  # Download Settings
   $magnolia_download_url = $magnolia::params::magnolia_download_url,
+
+  # Magnolia Properies
+  $magnolia.home                            = undef,
+  $magnolia.repositories.config             = undef,
+  $magnolia.repositories.home               = undef,
+  $magnolia.repositories.jackrabbit.config  = undef,
+  $magnolia.logs.dir                        = undef,
+  $magnolia.cache.startdir                  = undef,
+  $magnolia.upload.tmpdir                   = undef,
+  $magnolia.logs.dir                        = undef,
+  $magnolia.utf8.enabled                    = undef,
+  $magnolia.develop                         = undef,
+  $magnolia.update.auto                     = undef,
+  $magnolia.author.key.location             = undef,
 
   # Persistence Settings
 
