0586fb8f7d06f1313449b2795450f8c40c11a4d4
David Stauffer <david.stauffer@williams-forrest.com>
2016-04-11 10:18:56 -0400

    including limits and scoping service variables

--- manifests/config.pp
+++ manifests/config.pp
@@ -36,6 +36,13 @@
 #
 class magnolia::config inherits magnolia {
 
+  limits::fragment {
+    "*/soft/nofile":
+    value => "1024";
+   "*/hard/nofile":
+    value => "8192";
+  }
+
   file { $magnolia::cms_dir:
     ensure => directory,
     owner  => $magnolia::user,
